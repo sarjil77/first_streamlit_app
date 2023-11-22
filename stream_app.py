b6ab2cfabdf90d03f8702bffd1b323e93aa17706
sarjil77 <138225892+sarjil77@users.noreply.github.com>
2023-11-22 15:35:48 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -4,7 +4,7 @@ streamlit.title('My parents New Healthy Dinner')
 streamlit.header('Breakfast Menu') 
 streamlit.text('🥣 Omega 3 & bulberry oatmeal')
 streamlit.text('🥗 Kale , Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg') 
 streamlit.text('🥑🍞 Avocado Toast')
 
 

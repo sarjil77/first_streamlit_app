04b623ab60c3fa43ff9edee00aa653134c5b14f6
sarjil77 <138225892+sarjil77@users.noreply.github.com>
2023-11-23 19:12:06 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -49,6 +49,6 @@ streamlit.dataframe(my_data_row)
 
 
 # adding fruits 
-fruits_selected = streamlit.multiselect("which fruit would you like to add?",'apple')
 
-streamlit.write('Thanks for adding :','apple')
+add_my_fruit = streamlit.text_input('What fruit would you like information about?')
+streamlit.write('Thanks for adding :',add_my_fruit)

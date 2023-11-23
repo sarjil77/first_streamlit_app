0291a54ff9a43aa97fec61f1101bd95cda13565d
sarjil77 <138225892+sarjil77@users.noreply.github.com>
2023-11-23 23:06:09 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -33,21 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 # new section to display fruityvice api response 
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-      streamlit.error("Please select a fruit to get information.")
-  else:
-      back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
 
-except URLError as e:
-  streamlit.error()
-
-
-#dont run anything past here whilr we troubleshoot
-
-#streamlit.stop()
 
 import snowflake.connector
 
@@ -72,7 +58,16 @@ streamlit.text(my_data_row)
 
 
 
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 
+except URLError as e:
+  streamlit.error()
 
 
 
